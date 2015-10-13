6dc862024c1d6bbcff91a8a36d8b6554a5b86e02
Bala Chirtsabesan <balatest@live.com>
2015-10-13 16:43:02 -0700

    [Dev14_KBeta8_KPartyInvites] Added HomeController with views, styled with Bootstrap

---
+++ Dev14_KBeta8/KPartyInvites/src/KPartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KPartyInvites.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace KPartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+    }
+
+}
--- Dev14_KBeta8/KPartyInvites/src/KPartyInvites/Startup.cs
+++ Dev14_KBeta8/KPartyInvites/src/KPartyInvites/Startup.cs
@@ -13,6 +13,9 @@ namespace KPartyInvites
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add MVC services to the services container.
+            services.AddMvc();
+
         }
 
         public void Configure(IApplicationBuilder app)
@@ -20,9 +23,15 @@ namespace KPartyInvites
             // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

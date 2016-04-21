17f5d9ebd99da7cf16a68a6c592814abefdd7e46
Bala Chirtsabesan <balatest@live.com>
2016-04-21 16:13:35 -0700

    [P2PXprojToCsproj1] Added ClassLibraryOnNetFx2 and ConsoleApplicationOnNetFx1 to the solution

---
+++ Dev14_KRc2/release.b81/P2PXprojToCsproj1/ConsoleApplicationOnNetFx1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationOnNetFx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibraryOnNetFx1.Class1 c1 = new ClassLibraryOnNetFx1.Class1();
+            //ClassLibraryOnNetFx2.Class1 c2 = new ClassLibraryOnNetFx2.Class1();
+
+            Console.WriteLine(c1.GetHello());
+            //Console.WriteLine(c2.GetHello());
+
+            Console.WriteLine("Hello world");
+            
+        }
+    }
+}

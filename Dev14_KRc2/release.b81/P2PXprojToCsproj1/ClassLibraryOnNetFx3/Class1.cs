a7a909b72f6d686769ce010e4c5c1fae293f30bf
Bala Chirtsabesan <balatest@live.com>
2016-04-21 17:39:59 -0700

    [P2PXprojToCsproj1] Add project ClassLibraryOnNetFx3 targeting net452

---
+++ Dev14_KRc2/release.b81/P2PXprojToCsproj1/ClassLibraryOnNetFx3/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryOnNetFx3
+{
+    public class Class1
+    {
+        public string GetHello()
+        {
+            return "Hello from ClassLibraryOnNetFx3";
+        }
+    }
+}

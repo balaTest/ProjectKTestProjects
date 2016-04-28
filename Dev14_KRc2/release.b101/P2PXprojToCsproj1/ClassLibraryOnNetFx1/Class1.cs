e13652bc945adfba13ed4b1b267d2425543586cb
Bala Chirtsabesan <balatest@live.com>
2016-04-28 09:53:40 -0700

    [P2PXprojToCsproj1] Create solution, add some projects to the solution

---
+++ Dev14_KRc2/release.b101/P2PXprojToCsproj1/ClassLibraryOnNetFx1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryOnNetFx1
+{
+    public class Class1
+    {
+        public string GetHello()
+        {
+            return "Hello from ClassLibraryOnNetFx1";
+        }
+    }
+}

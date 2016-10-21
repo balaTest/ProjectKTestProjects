80fc90cdb453e2cf3507390803b20317ee23f830
Bala Chirtsabesan <balaTest@live.com>
2016-10-21 03:41:09 -0700

    [ClassLibraryOnNetStd16_1] Created class lib on .NET Standard 1.6

---
+++ Dev14_K101/Tooling202/ClassLibraryOnNetStd16_1/src/ClassLibraryOnNetStd16_1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassLibraryOnNetStd16_1
+{
+    public class Class1
+    {
+        public string GetHello()
+        {
+            return "Hello from class library";
+        }
+    }
+}

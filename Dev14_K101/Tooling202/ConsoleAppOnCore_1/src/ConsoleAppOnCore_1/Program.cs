1015b00220cf342489351b99140101157cf4f7eb
Bala Chirtsabesan <balaTest@live.com>
2016-10-21 03:37:08 -0700

    [ConsoleAppOnCore_1] Created Console Application (.NET Core)

---
+++ Dev14_K101/Tooling202/ConsoleAppOnCore_1/src/ConsoleAppOnCore_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOnCore_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}

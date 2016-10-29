f167ddf67e2bc856815fc6f66d4c8cf5d55cfc75
Bala Chirtsabesan <balatest@live.com>
2016-10-29 08:18:58 -0700

    [AspNetCoreWeb_IndAuth_WithAI_1] Created ASP.NET Core Web Application (.NET Core) with Web Application (Ind Auth) template and AppInsights enabled

---
+++ Dev14_K101/Tooling202/AspNetCoreWeb_IndAuth_WithAI_1/src/AspNetCoreWeb_IndAuth_WithAI_1/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWeb_IndAuth_WithAI_1.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ Dev14_K101/Tooling202/AspNetCoreWeb_IndAuth_WithAI_1/src/AspNetCoreWeb_IndAuth_WithAI_1/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWeb_IndAuth_WithAI_1.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
